4b61e5e5b46728dcd321cc9095a63c93635962fc
bepplinh <bep2702@gmail.com>
2026-04-15 09:24:13 +0700

    Delete scratch-patch.js

--- scratch-patch.js
+++
@@ -1,6 +0,0 @@
-import { readFileSync, writeFileSync } from 'fs';
-let file = readFileSync('backend/src/config/redis.js', 'utf8');
-if (!file.includes('family: 4,')) {
-  file = file.replace('maxRetriesPerRequest: null,', 'maxRetriesPerRequest: null,\n  family: 4,');
-  writeFileSync('backend/src/config/redis.js', file);
-}
